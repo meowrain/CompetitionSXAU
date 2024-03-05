b8b998d51b8d05474c025e2490d6974226ba12b7
meowrain <meowrain@126.com>
2024-03-05 19:22:05 +0800

    feat:使用css module

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Container } from "@mui/material";
 import "./App.css";
 import Todo from "./components/Todo";
+import PrimarySearchAppBar from "./components/AppBar/AppBarTodo";
 function App() {
   return (
-    <Container fixed>
+    <Container maxWidth="lg">
       <div>
+        <PrimarySearchAppBar />
         <Todo />
       </div>
     </Container>
